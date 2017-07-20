b81b3d682bcc2bac884648a982b46a77c1c7de00
saraswatmks <manisara0000@gmail.com>
2017-07-20 19:47:39 +0530

    add files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# ZS-Challenge-2017
+# ZS Young Data Scientist Challenge 2017
+
+This repository contains starter scripts shared during the 3 days hiring competition.
+This competition is organised by ZS Associates in participation with HackerEarth
+to hire final year college students for data scientist profile.
+
+**Challenge Date:** 21st July 2017 to 24th July 2017  <br />
+**Challenge Type:** Individual <br />
+**Challenge Link:** [Go to Challenge](https://www.hackerearth.com/challenge/competitive/zs-young-data-scientist-challenge-2017/)
+
+
+
+
+
---
+++ R_Starter.R
@@ -0,0 +1,39 @@
+
+# This starter script is meant to help you understand how you can make your first submission 
+# in the format as expected. This scripts predicts events based on the popular past events per patient.
+
+path <- "/home/manish/Desktop/ML_Challenge_Creatives/ZS YOUNG/YDS-dataset/"
+setwd(path)
+
+# load library
+library(data.table)
+
+
+# load and check data ---------------------------------------------------------------
+
+train <- fread("train.csv")
+test <- fread("test.csv")
+sample_sub <- fread("sample_submission.csv")
+
+head(train)
+head(test)
+
+str(train)
+str(test)
+
+# order data
+train <- train[order(PID)]
+test <- test[order(PID)]
+
+# Predicting future events based on popular past events per patient -------
+train_dcast <- dcast(data = train, PID ~ Event, length, value.var = "Event")
+
+# get top 10 events per row
+random_submit <- colnames(train_dcast)[-1][apply(train_dcast[,-c('PID'),with=F],1, function(x)order(-x)[1:10])]
+
+# create the submission file
+random_mat <- as.data.table((matrix(random_submit,ncol = 10, byrow = T)))
+colnames(random_mat) <- colnames(sample_sub)[-1]
+random_mat <- cbind(PID = test$PID, random_mat)
+fwrite(random_mat,"random_sub.csv")
+
